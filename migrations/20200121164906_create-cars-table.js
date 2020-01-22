57c0db58b94ce5e02c5f3c397e9783ce6c30b692
Reece Pierson <piersonreece@gmail.com>
2020-01-21 19:10:14 -0600

    created knex, db, etc. got migrations working

---
+++ migrations/20200121164906_create-cars-table.js
@@ -0,0 +1,21 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('cars', tbl => {
+        tbl.increments();
+        tbl.string('vin')
+          .unique()
+          .notNullable();
+        tbl.string('make')
+          .notNullable();
+        tbl.string('model')
+          .notNullable();
+        tbl.integer('mileage')
+          .notNullable();
+        tbl.string('transmissionType')
+        tbl.string('titleStatus')
+    })
+  };
+   
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('cars')
+  };
+  
